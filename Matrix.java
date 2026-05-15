ee6630d10658e111679aa7aa25c4a235cf19fabc
adrianhelllo <hetessyadrian@gmail.com>
2026-05-15 09:01:02 +0200

    fix matrix printing

--- Matrix.java
+++ Matrix.java
@@ -1,18 +1,20 @@
 public class Matrix {
     private int width;
     private int height;
-    private int[][] mat_arr = new int[height][width];
+    private int[][] mat_arr;
 
     public Matrix(int w, int h) {
         width = w;
         height = h;
+        mat_arr = new int[height][width];
     }
 
     public void printMatrix() {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                System.out.println(mat_arr[i][j]);
+                System.out.print(mat_arr[i][j] + " ");
             }
+            System.out.println();
         }
     }   
 }    
