9b684674a6fcf7a3b38d7e6f399f68a4d591567f
adrianhelllo <hetessyadrian@gmail.com>
2026-05-22 10:52:41 +0200

    add debug messages

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Matrix mat = new Matrix(5, 10);
-        mat.fillMatrix(0, 100);
+        // get user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Width: ");
+        final int X = Integer.parseInt(scanner.nextLine());
+        System.out.print("Height: ");
+        final int Y = Integer.parseInt(scanner.nextLine());
+
+        final int RAND_MIN = 0, RAND_MAX = 100;
         
-        mat.snakeHorizontal(1, 3, 5);
+        Matrix mat = new Matrix(Y, X, RAND_MIN, RAND_MAX);
+        mat.fillMatrix(RAND_MIN, RAND_MAX);
+        
+        // initialise loop variables
+        int bx_low = 0, bx_high = X;
+        int by_low = 0, by_high = Y;
+
+        while (bx_low != bx_high || by_low != by_high) {
+            mat.snakeVertical(bx_low, by_low, by_high);
+            mat.snakeHorizontal(by_high - 1, bx_low, bx_high);
+
+            by_high--;
+            bx_low--;
+
+            mat.snakeVertical(bx_high - 1, by_high, by_low);
+        }
     }
 }
--- Matrix.java
+++ Matrix.java
@@ -1,16 +1,21 @@
 public class Matrix {
     private String snake = "\u001b[0;32m"; // green color
+    private String clear = "\u001b[0m";
     private int width;
     private int height;
+    private int min_rand, max_rand;
     private int[][] mat_arr;
     private int[][] states;
+    private int max_digits;
 
     // define constructor
-    public Matrix(int w, int h) {
-        width = w;
-        height = h;
-        mat_arr = new int[height][width];
-        states = new int[height][width];
+    public Matrix(int w, int h, int min, int max) {
+        this.width = w;
+        this.height = h;
+        this.min_rand = min; max_rand = max;
+        this.mat_arr = new int[height][width];
+        this.states = new int[height][width];
+        this.max_digits = Integer.toString(max_rand).length();
     }
 
     public void printState() {
@@ -18,19 +23,30 @@ public class Matrix {
             for (int j = 0; j < width; j++) {
                 if (this.states[i][j] == 1) {
                     System.out.print(snake);
+                } else {
+                    System.out.print(clear);
                 }
-                System.out.print(mat_arr[i][j] + " ");
+
+                int cell = this.mat_arr[i][j];
+                int cell_digits = Integer.toString(cell).length();
+                
+                System.out.print(cell);
+                for (int k = 0; k <= this.max_digits - cell_digits; k++) {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
+
+        System.out.println(clear + "\n\n");
     }
 
     // fills matrix with random integer values between the min and max parameters, inclusive
     public void fillMatrix(int min, int max) {
         int rand;
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < this.height; i++) {
+            for (int j = 0; j < this.width; j++) {
                 // define random number between min and max
                 rand = (int) (Math.random() * (max - min + 1) + min); // max - min: the possible random of numbers, +1: we round downwards, +min: shift up by the minimum value
                 mat_arr[i][j] = rand;
@@ -43,8 +59,7 @@ public class Matrix {
         int dir_step = (bound_to > bound_from ) ? 1 : -1;
         for (int i = bound_from; i != bound_to; i += dir_step) {
             this.states[row][i] = 1;
-            System.out.print(mat_arr[row][i] + " ");
-
+            System.out.println("Snaking horizontally" + row + ", " + i);
             this.printState();
         }
     }
@@ -53,8 +68,7 @@ public class Matrix {
         int dir_step = (bound_to > bound_from) ? 1 : -1;
         for (int i = bound_from; i != bound_to; i += dir_step) {
             this.states[i][col] = 1;
-            System.out.println(mat_arr[i][col]);
-
+            System.out.println("Snaking vertically" + i + ", " + col); 
             this.printState();
         }
     }
